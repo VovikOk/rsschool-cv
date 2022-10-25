a81d4d44c241ec1513231b2acda85d4a95b966a1
VladimirOk <vovik-ok.87@ukr.net>
2022-10-25 20:20:42 +0300

    fix:fiften

--- script.js
+++ script.js
@@ -1,197 +1,175 @@
 //визначаємо розмір сторінки
-const pageWidth = document.documentElement.scrollWidth;
+/*const pageWidth = document.documentElement.scrollWidth;
 const pageHeight = document.documentElement.scrollHeight;
-
 //підключаємо Canvas
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 let  n = 16;
- 
-
-
-
-//задаємо значення сторони вікна Canvas
-let width = pageWidth *0.4;
-document.getElementById("myCanvas").width= width;
-document.getElementById("myCanvas").height= width;
-
-
-let lastEl;
-let numberStr = Math.sqrt(n); //визначаємо розмірність
-let widthNumber = Math.floor(width/numberStr);//визначаємо значення сторони клітинки
+let width = pageWidth *0.4;//задаємо значення сторони вікна Canvas
+let numberStr;//визначаємо розмірність
+let widthNumber;//визначаємо значення сторони клітинки
 let arrN = [];//масив чисел 
 let arr = [];// масив данних
 let arrEtalon = [];
-let arrF = [];
 let k = 0;//зберігаємо позицію цифри в масиві
+let zeroNum;//позиція пустої клітинки
 let x = 0;
 let y = 0;
 let f = 0;//цифра
 let clickXY;//координати кліку
 let clickNum;//цифра кліку
 let m; //попередня кількість клітин
-let numClick = 0;//кількість кліків за гру
-let p = n-1;
-document.getElementById("textarea").value = numClick;
+let numClick = 0;//кількість кліків за гру*/
 
-begin();
 
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+let pageWidth = document.documentElement.scrollWidth;  
+
+//загружаємо збережені дані або початкові
+const data = JSON.parse(localStorage.getItem('data')) || {   
+    n : 16,
+    width : pageWidth *0.4,
+    numberStr :0,
+    widthNumber:0,
+    arrN : [],
+    arr : [],
+    arrEtalon : [],
+    progres : [0],
+    k : 0,
+    zeroNum:0,
+    x : 0,
+    y : 0,
+    f : 0,
+    clickXY:0,
+    clickNum:0,
+    m:0,
+    numClick : 0,
+}
+
+document.getElementById("myCanvas").width= data.width;//задаємо ширину поля
+document.getElementById("myCanvas").height= data.width;//задаємо висоту поля
+document.getElementById("textarea").value = data.numClick;//показуємо кількість кліків
+let tex = document.getElementById('textarea');
+
+//продовжуєм гру
+if (localStorage.getItem('data')) {
+    start(); 
+}
+
+                                        //НОВА ГРА ПРИ ЗМІНІ РОЗМІРНОСТІ//
 // визначення розмірності
 document.getElementById("input").addEventListener('input', (event) => {
     const value = +event.target.value;
-    if (+value) { 
-       m = n;
-       f = 0;
-       x = 0;
-       y = 0;
-       k = 0;
-       numClick = 0;
-       //затираємо масиви
-       for (let i = 0; i < m; i++) {
-        arrN.pop(); 
-        arr.pop();    
-    }
-       n = value; 
-       ctx.clearRect(0,0, width, width);       
-       begin()    
+    if (+value) {       
+       data.n = value;              
+       begin();    
     }   
 })
-
 // формуємо дошку
 function begin() {
-numberStr = Math.trunc(Math.sqrt(n)); //визначаємо розмірність
-widthNumber = Math.floor(width/numberStr);//визначаємо значення сторони клітинки
-
+    data.m = data.n;
+    data.f = 0;
+    data.x = 0;
+    data.y = 0;
+    data.k = 0;
+    data.numClick = 0;
+   //затираємо масиви
+   for (let i = 0; i < data.m; i++) {
+    data.arrN.pop(); 
+    data.arr.pop(); 
+    data.arrEtalon=[]; 
+    ctx.clearRect(0,0, data.width, data.width);   
+}
+    data.numberStr = Math.trunc(Math.sqrt(data.n)); //визначаємо розмірність
+    data.widthNumber = Math.floor(data.width/data.numberStr);//визначаємо значення сторони клітинки
 //створюємо масив чисел
-for (let i = 1; i < n; i++) {
-    arrN.push(i); 
-    arrEtalon.push(i);    
+for (let i = 1; i < data.n; i++) {
+    data.arrN.push(i); 
+    data.arrEtalon.push(i);    
 }
-arrN.sort(() => Math.random() - 0.5);//сортуємо масив чисел
-
+data.arrEtalon.push('');
+data.arrN.sort(() => Math.random() - 0.5);//сортуємо масив чисел
 //створюємо масив данних
-for (let i = 1; i <= numberStr; i++) {
-    for (let j = 1; j <= numberStr; j++) {       
-       arr.push([x,y,arrN[f]]); 
-       f++;        
-       x = j * widthNumber;           
+for (let i = 1; i <= data.numberStr; i++) {
+    for (let j = 1; j <= data.numberStr; j++) {       
+        data.arr.push([data.x, data.y, data.arrN[data.f]]); 
+       data.f++;        
+       data.x = j * data.widthNumber;           
     } 
-    x = 0;
-    y = i * widthNumber;     
+    data.x = 0;
+    data.y = i * data.widthNumber;     
 }
-lastEl = arr.pop();//видаляємо останній елемент, координати пустого квадрату
-
+data.zeroNum = data.n-1;
+data.arr[data.zeroNum][2]='';//видаляємо цифру останного елемента,  пустого квадрату
 //малюємо дошку
-arr.map(discribe);
-//малюємо пусту клітинку
-discribeZero();
+data.arr.map(discribe);
+document.getElementById("textarea").value = '';
 }
 
-function discribeZero() {   
-    ctx.fillStyle = 'bisque';
-    ctx.fillRect(lastEl[0], lastEl[1], widthNumber, widthNumber);     
+//ГРА
+c.addEventListener('click', function(event) {
+    let clickX = Math.floor(event.offsetX/data.widthNumber)*data.widthNumber;
+    let clickY = Math.floor(event.offsetY/data.widthNumber)*data.widthNumber;    
+    data.clickXY = [clickX, clickY]; //визначаємо координати кліку    
+    //визначаємо цифру кліку
+    for (let i = 0; i < data.arr.length; i++) {
+        if (data.clickXY[0] === data.arr[i][0] && data.clickXY[1] === data.arr[i][1]) {
+            data.clickNum = data.arr[i][2];
+            data.k = i;// зберігаємо позицію цифри в масиві
+            break;
+        }               
+    }     
+  
+if ((data.clickXY[0] + data.widthNumber === data.arr[data.zeroNum][0] && data.clickXY[1]  === data.arr[data.zeroNum][1]) || 
+    (data.clickXY[0] - data.widthNumber === data.arr[data.zeroNum][0] && data.clickXY[1]  === data.arr[data.zeroNum][1]) ||
+    (data.clickXY[1] - data.widthNumber === data.arr[data.zeroNum][1] && data.clickXY[0]  === data.arr[data.zeroNum][0]) ||
+    (data.clickXY[1] + data.widthNumber === data.arr[data.zeroNum][1] && data.clickXY[0]  === data.arr[data.zeroNum][0])){
+      game();        
+}
+document.getElementById("textarea").value = data.numClick; 
+
+localStorage.setItem ('data', JSON.stringify(data));        //зберігаємо дані гри
+if(JSON.stringify(data.arrEtalon) === JSON.stringify(data.arrN)){ //перевіряемо перемогу  
+   
+    if (data.progres.length < 10) {
+        data.progres.push(data.numClick);
+    } else {
+        data.progres[10] = data.numClick;
+    }
+    data.progres.sort((a, b) => a - b); 
+
+    //задаємо правило запису нової операції з нової строки в полі виводу
+
+  document.getElementById('textarea').value += "\n";// перенос на новую строку
+  tex.scrollTop = tex.scrollHeight;// виставляем фокус на последней строке    
+    alert('end');
+}
+});
+
+function game() {
+    data.arr[data.zeroNum][2] = data.clickNum;//записуємо цифру кліку в пусту клітинку
+    data.arrN[data.zeroNum]= data.clickNum;
+    discribe(data.arr[data.zeroNum]);//перемальовуємо клітинку
+    ctx.clearRect(data.arr[data.k][0],data.arr[data.k][1], data.widthNumber, data.widthNumber);//стираємо стару клітинку     
+    data.arr[data.k][2]=''; 
+    data.arrN[data.k]='';  
+    data.zeroNum = data.k;             
+    data.clickXY = 0; 
+    data.numClick++;  
 }
 
 function discribe(e) {   
-   ctx.fillStyle = 'black';
-   ctx.strokeStyle = "white";
-   ctx.strokeRect(e[0], e[1], widthNumber, widthNumber,tag='133');
-   ctx.font = '24px serif';   
-   ctx.fillText(e[2], e[0]+(widthNumber/2), e[1]+(widthNumber/2));    
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(e[0], e[1], data.widthNumber, data.widthNumber,tag='133');
+    ctx.font = '18px serif';   
+    ctx.fillText(e[2], e[0]+(data.widthNumber/2), e[1]+(data.widthNumber/2));    
+ }
+
+function start() {
+data.arr.map(discribe);     
 }
 
 
-//гра
-c.addEventListener('click', function(event) {
-    let clickX = Math.floor(event.offsetX/widthNumber)*widthNumber;
-    let clickY = Math.floor(event.offsetY/widthNumber)*widthNumber;    
-    clickXY = [clickX, clickY]; //визначаємо координати кліку    
-    //визначаємо цифру кліку
-    for (let i = 0; i < arr.length; i++) {
-        if (clickXY[0] == arr[i][0] && clickXY[1] == arr[i][1]) {
-            clickNum = arr[i][2];
-            k = i;// зберігаємо позицію цифри в масиві
-            break;
-        }               
-    } 
-    
-  /*  if (clickXY[0] - widthNumber > 0 && clickXY[1]- widthNumber > 0 && clickXY[0] + widthNumber < width  && clickXY[y] + widthNumber < width) {}*/
-if (clickXY[0] + widthNumber === lastEl[0] && clickXY[1]  === lastEl[1]){
-    lastEl[2] = clickNum;//записуємо цифру кліку в пусту клітинку
-    discribe(lastEl);//перемальовуємо клітинку
-    arr[k][0]=lastEl[0];
-    arr[k][1]=lastEl[1];   
-    //arr[k][2]=''; 
-   // arr[k][2]=clickNum;        
-    lastEl[0] = clickXY[0];
-    lastEl[1] = clickXY[1];
-    lastEl[2] = '';
-    ctx.clearRect(lastEl[0],lastEl[1], widthNumber, widthNumber);//стираємо стару клітинку            
-    clickXY = 0; 
-    numClick++;                
-}
-if (clickXY[0] - widthNumber === lastEl[0] && clickXY[1]  === lastEl[1]){
-    lastEl[2] = clickNum;
-    discribe(lastEl);
-    arr[k][0]=lastEl[0];
-    arr[k][1]=lastEl[1];       
-    lastEl[0] = clickXY[0];
-    lastEl[1] = clickXY[1];
-    ctx.clearRect(lastEl[0],lastEl[1], widthNumber, widthNumber);
-    clickXY = 0;   
-    numClick++;          
-}
-if (clickXY[1] - widthNumber === lastEl[1] && clickXY[0]  === lastEl[0]){
-    lastEl[2] = clickNum;
-    discribe(lastEl);
-    arr[k][0]=lastEl[0];
-    arr[k][1]=lastEl[1];        
-    lastEl[0] = clickXY[0];
-    lastEl[1] = clickXY[1];
-    ctx.clearRect(lastEl[0],lastEl[1], widthNumber, widthNumber);
-    clickXY = 0;     
-    numClick++;     
-}
-if (clickXY[1] + widthNumber === lastEl[1] && clickXY[0]  === lastEl[0]){
-    lastEl[2] = clickNum;
-    discribe(lastEl);
-    arr[k][0]=lastEl[0];
-    arr[k][1]=lastEl[1];      
-    lastEl[0] = clickXY[0];
-    lastEl[1] = clickXY[1];
-    ctx.clearRect(lastEl[0],lastEl[1], widthNumber, widthNumber);
-    clickXY = 0;  
-    numClick++;        
-}
-document.getElementById("textarea").value = numClick;
-arrF[p] = clickNum;
-p = k;
 
-//
-
-  
-    
-     if(JSON.stringify(arrEtalon) === JSON.stringify(arrF)){
-        alert('end');
-     };
-
-//
-
-});
-
-
-
-
-
-
-
-
-
-/*
-document.addEventListener('click', function(event) {
-let c= event.target.context;
-    if (event.target.ctx.tag != undefined) { // якщо атрибут існує...
-        ctx.font = '24 serif';
-    }
-
-  });*/
